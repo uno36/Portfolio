022f103c9bed548babf154f0423356be6ae24f56
Emmanuel <emmanuelutofa@gmail.com>
2023-04-11 16:14:18 +0200

    changed background and font colour in the mobile-nav

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector('#open-menu');
 const mobileMenu = document.querySelector('.mobile-nav');
 const closeBtn = document.querySelector('#close-menu');
+const menuItems = document.querySelectorAll('#mobile-menu');
 
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('is-active');
@@ -9,4 +10,10 @@ menuBtn.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
   mobileMenu.classList.remove('is-active');
+});
+
+menuItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    mobileMenu.classList.remove('is-active');
+  });
 });
