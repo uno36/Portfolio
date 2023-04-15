e8cfcb0328b50f8f1fdf1282fd35237cba5928bf
Emmanuel <emmanuelutofa@gmail.com>
2023-04-15 21:48:31 +0200

    changes

--- script.js
+++ script.js
@@ -20,208 +20,126 @@ menuItems.forEach((item) => {
 
 const projects = [
   {
-    title: 'My Recent Works',
-  },
-  {
-    img1: 'images/pic.png',
-    img2: 'https://example.com/image2.jpg',
+    img: 'images/gymnast.png',
     title: 'Multi-Post Stories',
-    text: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-    buttons: ['CSS', 'HTML', 'Bootstrap', 'Ruby'],
-    tags: ['see project'],
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    tags: ['CSS', 'HTML', 'Bootstrap', 'RUby'],
+    button: 'See Project',
   },
   {
-    title: 'Profesional Art Printing Data',
-    text: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    buttons: ['HTML', 'Bootstrap', 'Ruby'],
+    title: 'Professional Art Printing Data',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    tags: ['HTML', 'Bootstrap', 'RUby'],
+    button: 'See Project',
   },
-
+  {
+    title: 'Professional Art Printing Data',
+    title1: 'Data Dashboard Healthcare',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    tags: ['HTML', 'Bootstrap', 'RUby'],
+    button: 'See Project',
+  },
+  {
+    title: 'Professional Art Printing Data',
+    title1: 'Website Potfolio',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    tags: ['HTML', 'Bootstrap', 'RUby'],
+    button: 'See Project',
+  },
+  {
+    title: 'Professional Art Printing Data',
+    title1: 'Profesional Art Printing Data More',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    tags: ['HTML', 'Bootstrap', 'RUby'],
+    button: 'See Project',
+  },
+  {
+    title: 'Professional Art Printing Data',
+    title1: 'Data Dashboard Healthcare',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    tags: ['HTML', 'Bootstrap', 'RUby'],
+    button: 'See Project',
+  },
+  {
+    title: 'Professional Art Printing Data',
+    title1: 'Website Portfolio',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    tags: ['HTML', 'Bootstrap', 'RUby'],
+    button: 'See Project',
+  },
 ];
 
-const works = document.getElementById('works');
+const projectsContainer = document.getElementById('works');
 
-projects.forEach((project, index) => {
-  const div = document.createElement('div');
+const projectsGrid = document.getElementById('grid-projects');
 
-  const openModalButtons = document.querySelectorAll('[data-modal-target]');
-  const closeModalButtons = document.querySelectorAll('[data-close-button]');
-  const overlay = document.getElementById('overlay');
+for (let i = 0; i < projects.length; i += 1) {
+  const project = projects[i];
 
-  function openModal(modal) {
-    if (modal == null) return;
-    modal.classList.add('active');
-    overlay.classList.add('active');
-  }
-
-  function closeModal(modal) {
-    if (modal == null) return;
-    modal.classList.remove('active');
-    overlay.classList.remove('active');
-  }
-
-  openModalButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      const modal = document.querySelector(button.dataset.modalTarget);
-      openModal(modal);
-    });
-  });
-
-  overlay.addEventListener('click', () => {
-    const modals = document.querySelectorAll('.modal.active');
-    modals.forEach((modal) => {
-      closeModal(modal);
-    });
-  });
-
-  closeModalButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      const modal = button.closest('.modal');
-      closeModal(modal);
-    });
-  });
-
-  if (index === 0) {
-    div.className = 'card-1';
-    div.innerHTML = `
-    <div class="flex">
-            <h1 class="recent">${project.title}</h1>
-            <hr class="hor" >
+  if (i === 0) {
+    const projectHTML = `
+      <div class="project1">
+      <div class="grid">
+        <div>
+        ${project.img ? `<img src="${project.img}" alt="${project.title}" class="gymnast">` : ''}
         </div>
-    `;
-  } else if (index === 1) {
-    div.className = 'card-2';
-    div.innerHTML = `
-    <div class="grid">
-      <div class="gymnast"></div>
-      <div class="text-container">
+        <div>
         <h2 class="recent1">${project.title}</h2>
-        <p class="recentp">${project.text}</p>
-      <div class="languages">
-          <ul>
-            <li><button class="works-btn">CSS</button>
-            <li><button class="works-btn">HTML</button>
-            <li><button class="works-btn">Bootstrap</button>
-            <li><button class="works-btn">Ruby</button>
-            </ul>
+        <p class="recentp" >${project.description}</p>
+        <ul class="langu">
+          ${project.tags.map((tag) => `<li class="works-btn">${tag}</li>`).join('')}
+        </ul>
+        <button class="see">${project.button}</button>
         </div>
-        <button class="see" data-modal-target="#modal" >${project.tags}</button>
       </div>
-    </div>
-    <div class="grid-projects">
-            <div class="see-proj">
-             <div class="projects-background" id="whitebg"> 
-                <div class="background">
-                <h2 >${project.title}</h2>
-                <p class="para">${project.text}</p>
-                <div class="project-btn">
-                    <ul class="btn-ul">
-                        <li><button class="projects-btn" type="button">HTML</button></li>
-                        <li><button class="projects-btn" type="button">Bootstrap</button></li>
-                        <li><button class="projects-btn" type="button">Ruby</button></li>
-                    </ul>
-                </div>
-            </div>
-            <button data-modal-target="#modal" class="sees-project" id="see-proj-btn">See project</button>
-       </div>
-        </div>
-
-        <div class="see-proj">
-        <div class="projects-background" id="bg2"> 
-            <div class="background">
-           <h2 class="projects-heading">Professional Art <br>
-            printing Data</h2>
-           <h2 class="Desktop-heading">Data Dashboard Healthcare</h2>
-           <p class="para">A daily selection of privately <br> personalized reads; no accounts or <br> sign-ups required. Has been the <br> industry's standard.</p>
-           <div class="project-btn">
-               <ul>
-                   <li><button class="projects-btn" type="button">HTML</button></li>
-                   <li><button class="projects-btn" type="button">Bootstrap</button></li>
-                   <li><button class="projects-btn" type="button">Ruby</button></li>
-               </ul>
-           </div>
-   </div>  
-         
-        </div>
-        <button data-modal-target="#modal" class="see-project">See project</button>
-   </div>
-   <div class="see-proj">
-        <div class="projects-background" id="bg3"> 
-           <div class="background">
-            <h2 class="projects-heading">Professional Art <br> printing Data</h2>
-            <h2 class="heading3" >website Portfolio</h2>
-           <p class="para">A daily selection of privately <br> personalized reads; no accounts or <br> sign-ups required. Has been the <br> industry's standard.</p>
-           <div class="project-btn">
-               <ul>
-                   <li><button class="projects-btn" type="button">HTML</button></li>
-                   <li><button class="projects-btn" type="button">Bootstrap</button></li>
-                   <li><button class="projects-btn" type="button">Ruby</button></li>
-               </ul>
-           </div>
-   </div>  
-         
-        </div>
-        <button data-modal-target="#modal" class="see-project">See project</button>
-   </div>
-   <div class="see-proj">
-    <div class="projects-background" id="bg4"> 
-       <div class="background">
-        <h2 class="projects-heading">Professional Art <br>printing Data</h2>
-        <h2 class="Desktop-heading">Profesional Art Printing Data More</h2>
-       <p class="para">A daily selection of privately <br> personalized reads; no accounts or <br> sign-ups required. Has been the <br> industry's standard.</p>
-       <div class="project-btn">
-           <ul>
-               <li><button class="projects-btn" type="button">HTML</button></li>
-               <li><button class="projects-btn" type="button">Bootstrap</button></li>
-               <li><button class="projects-btn" type="button">Ruby</button></li>
-           </ul>
-       </div>
-</div>  
-     
-    </div>
-    <button data-modal-target="#modal" class="see-project">See project</button>
-</div>
-<div class="see-proj">
-    <div class="projects-background" id="bg5" > 
-       <div class="background">
-        <h2 class="projects-heading">Professional Art <br> printing Data</h2>
-        <h2 class="Desktop-heading">Data Dashboard Healthcare</h2>
-       <p class="para">A daily selection of privately <br> personalized reads; no accounts or <br> sign-ups required. Has been the <br> industry's standard.</p>
-       <div class="project-btn">
-           <ul>
-               <li><button class="projects-btn" type="button">HTML</button></li>
-               <li><button class="projects-btn" type="button">Bootstrap</button></li>
-               <li><button class="projects-btn" type="button">Ruby</button></li>
-           </ul>
-       </div>
-</div>  
-     
-    </div>
-    <button data-modal-target="#modal" class="see-project">See project</button>
-</div>
-
-
-<div class="see-proj">
-    <div class="projects-background" id="bg6"> 
-       <div class="background">
-        <h2 class="projects-heading">Professional Art <br> printing Data</h2>
-            <h2 class="heading3" >website Portfolio</h2>
-       <p class="para">A daily selection of privately <br> personalized reads; no accounts or <br> sign-ups required. Has been the <br> industry's standard.</p>
-       <div class="project-btn">
-           <ul>
-               <li><button class="projects-btn" type="button">HTML</button></li>
-               <li><button class="projects-btn" type="button">Bootstrap</button></li>
-               <li><button class="projects-btn" type="button">Ruby</button></li>
-           </ul>
-       </div>
-    </div> 
-    </div>
-</div>
-  <button data-modal-target="#modal" class="see-project">See project</button>                    
-    </div>       
-    
-       
-  
+      </div>
     `;
+
+    projectsContainer.innerHTML += projectHTML;
+  } else if (i === 1) {
+    const project1HTML = `
+      <div class="grid-projects">
+      <div class="see-proj">
+        <div class="projects-background" id="whitebg">  
+        <button class="sees-project" id="see-proj-btn">${project.button}</button>
+                    
+        </div>
+      </div>  
+      `;
+    const projectElement = document.getElementById(`project-${i}`);
+    projectElement.addEventListener('mouseenter', () => {
+      const imageElement = document.getElementById(`project-img-${i}`);
+      const buttonElement = document.getElementById(`see-proj-btn-${i}`);
+      imageElement.classList.add('hover');
+      buttonElement.classList.add('hover');
+    });
+
+    projectElement.addEventListener('mouseleave', () => {
+      const imageElement = document.getElementById(`project-img-${i}`);
+      const buttonElement = document.getElementById(`see-proj-btn-${i}`);
+      imageElement.classList.remove('hover');
+      buttonElement.classList.remove('hover');
+    });
+
+    projectsGrid.innerHTML += project1HTML;
+  } else {
+    const projectHTML = `
+      <div class="see-proj">
+        <div class="projects-background" id="bg${i}"> 
+          <div class="background">
+            <h2 class="projects-heading">${project.title}</h2>
+            ${project.title1 ? `<h2 class="Desktop-heading">${project.title1}</h2>` : ''}
+            <p class="para">${project.description}</p>
+            <div class="project-btn">
+              <ul>${project.tags.map((tag) => `<li class="projects-btn">${tag}</li>`).join('')}</ul>
+            </div>
+          </div>  
+        </div>
+        <button class="see-project">${project.button}</button>
+      </div>
+    `;
+
+    projectsGrid.innerHTML += projectHTML;
   }
-  works.appendChild(div);
-});
+}
+
