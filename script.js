a521bd986dd45cb209ff8328426e9c70a35d596b
Emmanuel <emmanuelutofa@gmail.com>
2023-04-20 22:18:51 +0200

    added javascript for data-preservation

--- script.js
+++ script.js
@@ -194,3 +194,24 @@ form.addEventListener('submit', (event) => {
   localStorage.setItem('form-data', JSON.stringify(formData));
   form.submit();
 });
+
+const fullName = document.querySelector('#name');
+const feedback = document.querySelector('#email');
+const Message = document.querySelector('#user-text');
+
+function visitorData() {
+  const visitor = {
+    Name: fullName.value,
+    Email: feedback.value,
+    Message: Message.value,
+  };
+  localStorage.setItem('visitor', JSON.stringify(visitor));
+}
+
+[fullName, feedback, Message].forEach((input) => input.addEventListener('focusout', visitorData));
+const visitorDataExist = JSON.parse(localStorage.getItem('visitor'));
+if (visitorDataExist) {
+  fullName.value = visitorDataExist.Name;
+  feedback.value = visitorDataExist.Email;
+  Message.value = visitorDataExist.Message;
+}
